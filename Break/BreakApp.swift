6d7360f8d30a0d6a05e73b6f13d5b78773a11313
pedrohm <pedro.macedo@eemovel.com.br>
2023-03-11 11:13:57 -0300

    Initial Commit

---
+++ Break/BreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakApp.swift
+//  Break
+//
+//  Created by Pedro Henrique Macedo on 11/03/23.
+//
+
+import SwiftUI
+
+@main
+struct BreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
