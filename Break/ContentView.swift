bfecf860760d16ac97c3e74f52721ad242367a78
pedrohm <pedro.macedo@eemovel.com.br>
2023-12-07 00:38:25 -0300

    placing images and text in zstack on the carousel

--- Break/ContentView.swift
+++ Break/ContentView.swift
@@ -5,46 +5,54 @@
 //  Created by Pedro Henrique Macedo on 11/03/23.
 //
 
-//import SwiftUI
-//
-//struct ContentView: View {
-//    var body: some View {
-//        VStack {
-//            Image("koenigsegg-gemera")
-//                .imageScale(.small)
-//                .foregroundColor(.green)
-//            Text("Hi!")
-//            Image(systemName: "scribble.variable")
-//                .imageScale(.large)
-//                .foregroundColor(.accentColor)
-//            Text("Hello, world!")
-//                .bold()
-//        }
-//        .padding()
-//    }
-//}
-//
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
-
 import SwiftUI
 
+class ViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.backgroundColor = .red
+        imageView.image = UIImage(named: "ford-mustang")
+        imageView.contentMode = .scaleAspectFit
+    }
+}
+
 struct ContentView: View {
+    
     var body: some View {
         NavigationView {
             VStack {
                 // Carousel
                 ScrollView(.horizontal, showsIndicators: false) {
                     HStack(spacing: 20) {
+//                        ForEach(1...3, id: \.self) { num in
+//                            Text("Card \(num)")
+//                                .frame(width: 340, height: 350)
+//                                .background(Color.white.gradient)
+////                                .background(imageView)
+//                                .cornerRadius(25)
+//                                .foregroundStyle(.gray)
+//                                .font(.headline)
+//                                .shadow(color: Color(white: 0.9, opacity: 0.99), radius: 4, x: 3, y: 3)
+//                        }
                         ForEach(1...3, id: \.self) { num in
-                            Text("Card \(num)")
-                                .frame(width: 340, height: 350)
-                                .background(Color.purple)
-                                .cornerRadius(25)
-                        }
+                                                    ZStack {
+                                                        Image("ford-mustang") // Replace with your image
+                                                            .resizable()
+                                                            .scaledToFill()
+                                                            .frame(width: 340, height: 350)
+                                                            .shadow(color: Color(white: 0.9, opacity: 0.5), radius: 8, x: 3, y: 3)
+                                                        Text("Card \(num)")
+                                                    }
+                                                    .frame(width: 340, height: 350)
+                                                    .background(Color.white.gradient)
+                                                    .cornerRadius(10)
+                                                    .foregroundStyle(.white)
+                                                    .font(.title)
+                                                    .shadow(color: Color(white: 0.5, opacity: 0.5), radius: 8, x: 3, y: 3)
+                                                }
                     }
                     .padding()
                 }
@@ -57,17 +65,26 @@ struct ContentView: View {
                 Spacer()
 
                 // Footer
-                Text("Contact Us")
-                    .padding()
+                Button(action: {
+                    // Handle button action
+                }) {
+                    Text("Contact Us")
+                        .foregroundColor(.white)
+                        .frame(minWidth: 100, maxWidth: 250)
+                        .padding()
+                        .background(Color.blue.gradient)
+                        .cornerRadius(50)
+                }
+                .padding()
             }
             .navigationBarItems(trailing: Button("Sign In") {
                 // Handle sign-in action
             })
             .navigationBarTitle("Home", displayMode: .inline)
+            .background(Color.clear.edgesIgnoringSafeArea(.all))
         }
     }
 }
-
 
 
 struct Previews_ContentView_Previews: PreviewProvider {
