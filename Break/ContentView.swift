49ae5825acc54fe1fd026939e704ec88b599e329
pedrohm <pedro.macedo@eemovel.com.br>
2023-12-06 23:11:09 -0300

    Adding frontend base

--- Break/ContentView.swift
+++ Break/ContentView.swift
@@ -5,22 +5,74 @@
 //  Created by Pedro Henrique Macedo on 11/03/23.
 //
 
+//import SwiftUI
+//
+//struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            Image("koenigsegg-gemera")
+//                .imageScale(.small)
+//                .foregroundColor(.green)
+//            Text("Hi!")
+//            Image(systemName: "scribble.variable")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//            Text("Hello, world!")
+//                .bold()
+//        }
+//        .padding()
+//    }
+//}
+//
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                // Carousel
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 20) {
+                        ForEach(1...3, id: \.self) { num in
+                            Text("Card \(num)")
+                                .frame(width: 340, height: 350)
+                                .background(Color.purple)
+                                .cornerRadius(25)
+                        }
+                    }
+                    .padding()
+                }
+
+                // Lorem Ipsum Text
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+                    .padding()
+                    .lineLimit(3)
+
+                Spacer()
+
+                // Footer
+                Text("Contact Us")
+                    .padding()
+            }
+            .navigationBarItems(trailing: Button("Sign In") {
+                // Handle sign-in action
+            })
+            .navigationBarTitle("Home", displayMode: .inline)
         }
-        .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+
+
+struct Previews_ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
